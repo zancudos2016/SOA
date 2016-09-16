e788416ae18adda875e7530fdbe20e17b47df6d0
ivanvelarde1980 <isvelarder@gmail.com>
2016-09-16 17:34:40 -0500

    Ivan Velarde

---
+++ WebApp/js/index.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    $(".input").focusin(function () {
+        $(this).find("span").animate({ "opacity": "0" }, 200);
+    });
+
+    $(".input").focusout(function () {
+        $(this).find("span").animate({ "opacity": "1" }, 300);
+    });
+
+    $("#cmbSend").click(function () {
+        $.post(
+            "WASECURITYG0001_USUA",
+            { COD_USUA: $("#Usuario").val(), ALF_PASS: $("#Password").val() })
+          .done(function (oBe) {
+              if (!oBe.IND_ERRO) {
+                  $(location).attr("href", oBe.ALF_URLL);
+              }
+              else {
+                  DevExpress.ui.notify(oBe.ALF_ERRO, "error", 1500);
+              }
+          });
+    });
+});
