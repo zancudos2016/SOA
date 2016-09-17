3c62c3566ef0883b534cf54f51e09d73ba2c692a
ivanvelarde1980 <isvelarder@gmail.com>
2016-09-16 20:53:55 -0500

    IvanVelarde

---
+++ Entidades/SHMC_EMPL.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class SHMC_EMPL
+    {
+        public int COD_TECN { get; set; }
+        public string ALF_EMPL { get; set; }
+    }
+}
---
+++ Entidades/SHMC_USUA.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class SHMC_USUA
+    {
+        public string COD_USUA { get; set; }
+        public int COD_TECN { get; set; }
+        public string ALF_PASS { get; set; }
+        public string ALF_TELE { get; set; }
+        public bool IND_INST { get; set; }
+        public int NUM_TOKE { get; set; }
+    }
+}
