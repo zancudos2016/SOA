1b1c5a36021e97d2658e576662d613502602eea6
ivanvelarde1980 <isvelarder@gmail.com>
2016-09-17 16:18:11 +0900

    IvanVelarde

--- WebApp/Views/Atenciones/atenciones.js
+++ WebApp/Views/Atenciones/atenciones.js
@@ -1,7 +1,6 @@
 ﻿$(document).ready(function () {
     $.post(
-        "../../WASECURITYG0002_SESI",
-        { NUM_ACCI: 1 })
+        "../../WAP0002_USUA")
       .done(function (oBe) {
           if (!oBe.IND_ERRO) {
               $("#mainContent").css("visibility", "visible");
@@ -41,16 +40,20 @@
         height:25
     });
     //--------------------------------------------------------------------------
+    //TIPOS DE ATENCIÓN
+    //--------------------------------------------------------------------------
+    var COD_TIPO_ATEN = [{ COD_TIPO: 1, ALF_TIPO: "Instalación" }, { COD_TIPO: 2, ALF_TIPO: "Desinstalación" }, { COD_TIPO: 3, ALF_TIPO: "Incidencia" }]
+    //--------------------------------------------------------------------------
     //INICIALIZAR CONTROLES PARA LA VENTANA AGREGAR USUARIO
     //--------------------------------------------------------------------------
-    function clickSavepopupUser() {
+    function clickSavepopupAtenciones() {
         $.isLoading();
-        $.post('../../WAS0003_USUA', {
-            COD_USUA:$("#txtCOD_USUA").dxTextBox("instance").option("value"),
-            ALF_NOMB:$("#txtALF_NOMB").dxTextBox("instance").option("value"),
-            ALF_EMAI:$("#txtALF_EMAI").dxTextBox("instance").option("value"),
-            ALF_PASS:$("#txtALF_PASS").dxTextBox("instance").option("value"),
-            ALF_TELE: $("#txtALF_TELE").dxTextBox("instance").option("value"),
+        $.post('../../WA0002_ATEN', {
+            COD_ATEN:$("#txtCOD_ATEN").dxTextBox("instance").option("value"),
+            ALF_COME:$("#txtALF_COME").dxTextBox("instance").option("value"),
+            COD_TIPO:$("#cmbCOD_TIPO").dxTextBox("instance").option("value"),
+            FEC_ATEN:$("#deFEC_ATEN").dxTextBox("instance").option("value"),
+            ALF_COME: $("#meALF_COME").dxTextBox("instance").option("value"),
             NUM_ACCI:Action
         })
         .done(function (data) {
@@ -58,7 +61,7 @@
                 DevExpress.ui.notify(data.ALF_ERRO, "error", 1500);
             }
             else {
-                popupUser.hide();
+                popupAtenciones.hide();
                 DevExpress.ui.notify(data.ALF_ERRO, "success", 1500);
                 UsersListInitialize();
             }
@@ -66,46 +69,46 @@
         });
     }
 
-    var clickClosepopupUser = function () {
-        popupUser.hide();
+    var clickClosepopupAtenciones = function () {
+        popupAtenciones.hide();
     }
-    var buttonItemspopupUser = [
-        { toolbar: 'bottom', location: 'after', widget: 'button', options: { hint: 'Guardar', icon: 'save', width: 25, disabled: false, onClick: clickSavepopupUser } },
-        { toolbar: 'bottom', location: 'after', widget: 'button', options: { hint: 'Cerrar', icon: 'close', width: 25, disabled: false, onClick: clickClosepopupUser } },
+    var buttonItemspopupAtenciones = [
+        { toolbar: 'bottom', location: 'after', widget: 'button', options: { hint: 'Guardar', icon: 'save', width: 25, disabled: false, onClick: clickSavepopupAtenciones } },
+        { toolbar: 'bottom', location: 'after', widget: 'button', options: { hint: 'Cerrar', icon: 'close', width: 25, disabled: false, onClick: clickClosepopupAtenciones } },
     ];
-    var popupUser_options = {
+    var popupAtenciones_options = {
         title: 'Agregar usuario',
         showTitle: true,
-        buttons: buttonItemspopupUser,
-        width: 500,
+        buttons: buttonItemspopupAtenciones,
+        width: 600,
         height: 250
     }
-    var popupUser = $("#popupUser").dxPopup(popupUser_options).dxPopup("instance");
+    var popupAtenciones = $("#popupAtenciones").dxPopup(popupAtenciones_options).dxPopup("instance");
     //--------------------------------------------------------------------------
     //DEFINICIÓN DE FUNCIONES PARA LA BARRA DE HERRAMIENTAS
     //--------------------------------------------------------------------------
     var Action = 1;
     function cmdNew() {
-        popupUser.show();
+        popupAtenciones.show();
         Action = 1;
-        $("#txtCOD_USUA").dxTextBox({ width: 100, readOnly: false,value:"" });
-        $("#txtALF_NOMB").dxTextBox({ width: 350, value: "" });
-        $("#txtALF_EMAI").dxTextBox({ width: 350, value: "" });
-        $("#txtALF_PASS").dxTextBox({ width: 150, mode: 'password', value: "" });
-        $("#txtALF_TELE").dxTextBox({ width: 150, value: "" });
+        $("#txtCOD_ATEN").dxTextBox({ width: 100, readOnly: false,value:"" });
+        $("#txtALF_COME").dxTextBox({ width: 350, value: "" });
+        $("#cmbCOD_TIPO").dxSelectBox({ width: 350, value: "", dataSource: COD_TIPO_ATEN,displayExpr:"ALF_TIPO", displayValue:"COD_TIPO"  });
+        $("#deFEC_ATEN").dxDateBox({ width: 150, value: new Date() });
+        $("#meALF_COME").dxTextArea({ width: 150, height:30, value: "" });
     }
     function cmdEdit() {
         if (Dato.COD_USUA === undefined) {
             DevExpress.ui.notify("Debe seleccionar un registro.", "error", 1500);
         }
         else {
-            popupUser.show();
+            popupAtenciones.show();
             Action = 2;
-            $("#txtCOD_USUA").dxTextBox({ width: 100, readOnly: true, value:Dato.COD_USUA });
-            $("#txtALF_NOMB").dxTextBox({ width: 350, value: Dato.ALF_NOMB });
-            $("#txtALF_EMAI").dxTextBox({ width: 350, value: Dato.ALF_EMAI });
-            $("#txtALF_PASS").dxTextBox({ width: 150, mode: 'password', value: Dato.ALF_PASS });
-            $("#txtALF_TELE").dxTextBox({ width: 150, value: Dato.ALF_TELE });
+            $("#txtCOD_ATEN").dxTextBox({ width: 100, readOnly: true, value:Dato.COD_ATEN });
+            $("#txtALF_COME").dxTextBox({ width: 350, value: Dato.ALF_COME });
+            $("#cmbCOD_TIPO").dxSelectBox({ width: 350, value: Dato.COD_TIPO, dataSource: COD_TIPO_ATEN, displayExpr: "ALF_TIPO", displayValue: "COD_TIPO" });
+            $("#deFEC_ATEN").dxDateBox({ width: 150, value: new Date(Dato.FEC_ATEN) });
+            $("#meALF_COME").dxTextArea({ width: 150, height: 30, value: Dato.ALF_OBSE });
         }
     }
 
@@ -220,7 +223,7 @@
     //--------------------------------------------------------------------------
     //DEFINICIÓN DE LA GRILLA PRINCIPAL
     //--------------------------------------------------------------------------
-    var gdvUsers_options = {
+    var gdvAtenciones_options = {
         columns: [{
                         dataField: 'COD_USUA',
                         caption: 'Usuario',
@@ -274,12 +277,12 @@
     function UsersListInitialize() {
         $.isLoading();
         $.post(
-            "../../WAG0002_USUA",
+            "../../WA0001_ATEN",
             { NUM_ACCI:1 })
           .done(function (oBe) {
               if (!oBe.IND_ERRO) {
-                  gdvUsers_options.dataSource = oBe.LST_USUA;
-                  $("#gdvUsers").dxDataGrid(gdvUsers_options);
+                  gdvAtenciones_options.dataSource = oBe.LST_ATEN;
+                  $("#gdvAtenciones").dxDataGrid(gdvAtenciones_options);
               }
               else {
                   DevExpress.ui.notify(oBe.ALF_ERRO, "error", 1500);
