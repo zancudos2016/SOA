cd323ab6a071f9587ca91c8fc52a9b825ceaeed0
Alexander <alexander112330@gmail.com>
2016-09-16 23:42:59 -0500

    Servicio, Atenciones

---
+++ WcfServices/Atenciones.svc.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using WcfServices.Errores;
+using WcfServices.Persistencia;
+
+namespace WcfServices
+{
+    public class Atenciones : IAtenciones
+    {
+        private AtencionDAO atencionDAO = new AtencionDAO();
+
+        public Entidades.SHMC_ATEN CrearAtencion(Entidades.SHMC_ATEN atencionACrear)
+        {
+            if (atencionDAO.Obtener(atencionACrear.COD_ATEN) != null) //Ya existe
+            {
+                throw new FaultException<GeneralException>(
+                    new GeneralException()
+                    {
+                        Codigo = "101",
+                        Descripcion = "La atención ya existe."
+                    },
+                    new FaultReason("Error al intentar creación."));
+            }
+            return atencionDAO.Crear(atencionACrear);
+        }
+
+        public Entidades.SHMC_ATEN ObtenerAtencion(int COD_ATEN)
+        {
+            return atencionDAO.Obtener(COD_ATEN);
+        }
+
+        public Entidades.SHMC_ATEN ModificarAtencion(Entidades.SHMC_ATEN atencionAModificar)
+        {
+            return atencionDAO.Modificar(atencionAModificar);
+        }
+
+        public int EliminarAtencion(int COD_ATEN)
+        {
+            int eliminado = atencionDAO.Eliminar(COD_ATEN);
+            if (eliminado == 0) //No existe
+            {
+                throw new FaultException<GeneralException>(
+                    new GeneralException()
+                    {
+                        Codigo = "102",
+                        Descripcion = "La atención no existe."
+                    },
+                    new FaultReason("Error al intentar eliminar."));
+            }
+            return eliminado;
+        }
+
+        public List<Entidades.SHMC_ATEN> ListarAtenciones()
+        {
+            return atencionDAO.Listar();
+        }
+    }
+}
