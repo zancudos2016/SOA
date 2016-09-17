332ccea9341dd9bbc5fecd9b994396d448a1d067
ivanvelarde1980 <isvelarder@gmail.com>
2016-09-17 12:31:07 +0900

    IvanVelarde

---
+++ AccesoDatos/DASNMC_USUA.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using System.Data.Common;
+using Entidades;
+
+namespace AccesoDatos
+{
+    public class DASHMC_USUA
+    {
+        private DAGenerics oDg;
+        private Database oDb;
+        private DbConnection oCon;
+        /// <summary>
+        /// CONTRUCTOR DE LA CLASE
+        /// </summary>
+        public DASHMC_USUA()
+        {
+            oDg = new DAGenerics("CSDEMO");
+        }
+        /// <summary>
+        /// OBTENER EL USUARIO LOGEADO
+        /// </summary>
+        /// <param name="oBe"></param>
+        /// <returns></returns>
+        public IDataReader Get_AP0001SNPR_LOGI_LIST(SHMC_USUA oBe)
+        {
+            try
+            {
+                oDb = oDg.getDataBase();
+                oCon = oDg.getConnection();
+                if (oCon.State == ConnectionState.Closed) oCon.Open();
+                var ocmd = oDb.GetStoredProcCommand("AP0001SNPR_LOGI_LIST", oBe.COD_USUA,
+                                                                            oBe.ALF_PASS);
+                ocmd.CommandTimeout = 2000;
+                var odr = oDb.ExecuteReader(ocmd);
+                return (odr);
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException(ex.Message);
+            }
+            finally
+            {
+                oCon.Close();
+            }
+        }
+    }
+}
---
+++ LogicaNegocio/LNSHMC_USUA.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccesoDatos;
+using Entidades;
+using System.Collections;
+using ResultSetMappers;
+
+namespace LogicaNegocio
+{
+    public class LNSHMC_USUA
+    {
+        private DASHMC_USUA oDa;
+        public LNSHMC_USUA()
+        {
+            oDa = new DASHMC_USUA();
+        }
+        /// <summary>
+        /// OBTENER EL USUARIO LOGEADO
+        /// </summary>
+        /// <param name="oBe"></param>
+        /// <returns></returns>
+        public List<SHMC_USUA> Get_AP0001SNPR_LOGI_LIST(SHMC_USUA oBe)
+        {
+            try
+            {
+                using (var odr = oDa.Get_AP0001SNPR_LOGI_LIST(oBe))
+                {
+                    var olst = new List<SHMC_USUA>();
+                    ((IList)olst).LoadFromReader<SHMC_USUA>(odr);
+                    return (olst);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException(ex.Message);
+            }
+        }
+    }
+}
