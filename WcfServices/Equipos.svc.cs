765701181111057ba17437381a4e5867a2171e91
Alexander <alexander112330@gmail.com>
2016-09-24 15:47:42 -0500

    Equipos

---
+++ WcfServices/Equipos.svc.cs
@@ -0,0 +1,31 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using WcfServices.Persistencia;
+
+namespace WcfServices
+{
+    public class Equipos : IEquipos
+    {
+        private EquipoDAO equipoDAO = new EquipoDAO();
+
+        public SHMC_EQUI ObtenerAtencion(int COD_EQUI)
+        {
+            return equipoDAO.Obtener(COD_EQUI);
+        }
+
+        public SHMC_EQUI ModificarAtencion(SHMC_EQUI equipoAModificar)
+        {
+            return equipoDAO.Modificar(equipoAModificar);
+        }
+
+        public List<Entidades.SHMC_EQUI> ListarAtenciones(int COD_TECN)
+        {
+            return equipoDAO.Listar(COD_TECN);
+        }
+    }
+}
---
+++ WcfServices/IEquipos.cs
@@ -0,0 +1,24 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using WcfServices.Errores;
+
+namespace WcfServices
+{
+    [ServiceContract]
+    public interface IEquipos
+    {
+        [OperationContract]
+        SHMC_EQUI ObtenerAtencion(int COD_EQUI);
+
+        [OperationContract]
+        SHMC_EQUI ModificarAtencion(SHMC_EQUI equipoAModificar);
+
+        [OperationContract]
+        List<SHMC_EQUI> ListarAtenciones(int COD_TECN);
+    }
+}
