89af473cf83527098f4b5ddf57a2354be215f8dd
Ralfred <Ralfred@RALAP>
2016-09-24 17:39:07 -0500

    Add Test Rest Atenciones

---
+++ TestServices/RestTestAtenciones.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestServices
+{
+    [TestClass]
+    public class RestTestAtenciones
+    {
+        
+        
+    }
+}
